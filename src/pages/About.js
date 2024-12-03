746e005c10a6c846af6f6a8ddfe0919954520dfd
Romildo A. Lima Jr. <odlimor.sevla@gmail.com>
2024-12-03 20:29:19 -0300

    Finalizando o context API com React.

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,13 +1,15 @@
 import { useContext } from "react";
 
 import { CounterContext } from "../context/CounterContext";
+import { useTitleColorContext } from "../hooks/useTitleColorContext";
 
 
 const About = () => {
+  const { color } = useTitleColorContext();
   const { counter } = useContext(CounterContext);
   return (
     <div>
-      <h1>About</h1>
+      <h1 style={{ color: color }}>About</h1>
       <p>Valor do contador.: {counter}</p>
     </div>
   )
